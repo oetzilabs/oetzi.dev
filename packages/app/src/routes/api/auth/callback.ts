59b2c89b31466cef52841b5bdcb57759fc129381
Özgür Isbert <oezguerisbert@gmail.com>
2023-10-13 23:59:06 +0200

    fix: auth now?

--- packages/app/src/routes/api/auth/callback.ts
+++ packages/app/src/routes/api/auth/callback.ts
@@ -1,19 +1,19 @@
 import { APIEvent, json, redirect, serializeCookie } from "solid-start";
 import fetch from "node-fetch";
 
-export const GET = async (event: APIEvent) => {
+export const POST = async (event: APIEvent) => {
   const url = new URL(event.request.url);
-  const code = url.searchParams.get("code");
-  if (!code) {
-    return json({ error: `No code`, url, h: event.params }, { status: 400 });
+  const { token } = await event.request.json();
+  if (!token) {
+    return json({ error: `No token` }, { status: 400 });
   }
   const response = await fetch(`${import.meta.env.VITE_AUTH_URL}/token`, {
     method: "POST",
     body: new URLSearchParams({
       grant_type: "authorization_code",
       client_id: "github",
-      code,
-      redirect_uri: `${url.origin}${url.pathname}`,
+      code: token,
+      redirect_uri: `${url.origin}/auth`,
     }),
   }).then((r) => r.json() as Promise<{ access_token: string }>);
   const c = serializeCookie("session", response.access_token, {
@@ -22,12 +22,17 @@ export const GET = async (event: APIEvent) => {
     maxAge: 60 * 60 * 24 * 7,
   });
 
-  return redirect("/", {
-    // redirect to the frontend
-    status: 302,
-    headers: {
-      Authorization: `Bearer ${response.access_token}`,
-      "Set-Cookie": c,
+  return json(
+    {
+      access_token: response.access_token,
     },
-  });
+    {
+      // redirect to the frontend
+      status: 302,
+      headers: {
+        Authorization: `Bearer ${response.access_token}`,
+        "Set-Cookie": c,
+      },
+    }
+  );
 };
--- packages/app/src/routes/auth.tsx
+++ packages/app/src/routes/auth.tsx
@@ -1,21 +1,35 @@
+import { createMutation, createQuery } from "@tanstack/solid-query";
 import { createEffect } from "solid-js";
 import { useLocation } from "solid-start";
+import { Queries } from "../utils/api/queries";
 
-export const AuthPage = () => {
+const AuthPage = () => {
   const { hash } = useLocation();
 
-  createEffect(() => {
-    const token = hash.split("token=")[1];
-    if (token) {
-      document.cookie = `session=${token}; path=/;`;
+  const x = createMutation((token: string) => {
+    return fetch("/api/auth/callback", {
+      body: JSON.stringify({ token }),
+      method: "POST",
+    }).then((res) => res.json());
+  });
+
+  createEffect(async () => {
+    const token = hash.split("#")[1].split("=")[1];
+    const session_set = await x.mutateAsync(token);
+    if (session_set) {
+      console.log("session set", session_set);
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 5000);
     }
-    window.location.href = "/";
   });
 
   return (
-    <div>
+    <div class="container flex flex-col gap-2.5 items-center justify-center p-10">
       <span>Loggin you in...</span>
       <span>Redirecting automatically...</span>
     </div>
   );
 };
+
+export default AuthPage;
