53c1ee5e8bfb25aed7a7fd7394b3db77dcb31f14
Özgür Isbert <oezguerisbert@gmail.com>
2023-10-13 14:57:33 +0200

    chores: added disclaimer

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div class="flex container mx-auto flex-col gap-4 py-10">
-      <div class="flex flex-col gap-4"></div>
+      <div class="flex flex-col items-center justify-center gap-4 p-20">
+        This app is currenyly under development. Please check back later.
+      </div>
     </div>
   );
 }
