41ae8193f8ada592e7e3577fe8b30da662f9c2da
Özgür Isbert <oezguerisbert@gmail.com>
2023-11-05 12:14:27 +0100

    feat: new logo added

--- packages/app/src/components/PublicProject.tsx
+++ packages/app/src/components/PublicProject.tsx
@@ -90,8 +90,13 @@ export const PublicProject = (props: PublicProjectProps) => {
       </div>
       <div class="flex flex-col gap-1 p-4 border-b border-neutral-300 dark:border-neutral-800">
         <div class="flex flex-row items-center justify-between gap-2.5">
-          <span class="text-md font-medium select-none">Created since {dayjs(props.project.createdAt).fromNow()}</span>
-          <div class="w-3 h-3 rounded-full bg-teal-500"></div>
+          <span class="text-md font-medium select-none">Created {dayjs(props.project.createdAt).fromNow()}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
+            <line x1="16" x2="16" y1="2" y2="6" />
+            <line x1="8" x2="8" y1="2" y2="6" />
+            <line x1="3" x2="21" y1="10" y2="10" />
+          </svg>
         </div>
       </div>
       <div class="flex flex-col gap-1 ">
